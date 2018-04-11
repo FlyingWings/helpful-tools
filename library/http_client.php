8c957997e4620ea1dbc0be60a87ddedc6f26799d
Alex <591647088@qq.com>
2018-04-11 18:58:53 +0800

    Minor: update http library && unique set added

--- library/http_client.php
+++ library/http_client.php
@@ -9,8 +9,7 @@
 class HTTP_client{
     public $client;
     public $cookies;
-    public function __construct($cookie=false, $base_url=""){
-//        $this->client = new \GuzzleHttp\Client();
+    public function __construct($base_url="", $cookie = false){
         if($cookie){
             $cookie_file = DATA.'/cookie/'.sha1($base_url);
             $content = file_get_contents($cookie_file);
@@ -54,4 +53,24 @@ class HTTP_client{
         ]);
         return $response;
     }
+
+//    public function crawl_images($url){
+//        $page = $this->client->request("GET", $url);
+//        if($page->getStatusCode() == "200"){
+//            $html = $page->getBody()->getContents();
+////            dd($html);
+//            $regex = "/(?<=src=\")[^\"]+\"(?= title)/";
+//            preg_match_all($regex, $html, $all);
+//            $i=0;
+//            foreach($all[0] as $pic_url){
+////                var_dump("https:".$pic_url);continue;
+//                $page = $this->client->requestAsync("GET", "https:".$pic_url);
+//                $page->
+//                dd($page->getState());
+//                file_put_contents(DATA."/log/image_{$i}.jpg", $page);
+//                $i++;
+//            }
+//
+//        }
+//    }
 }
---
+++ library/unique_set.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 18-4-11
+ * Time: 上午10:58
+ */
+
+class UniqueSet
+{
+    protected $set;
+    public function __construct(){
+        $this->set = [];
+    }
+
+    public function add($obj){
+        if(in_array($this->set, $obj)){
+            return 1;
+        }else{
+            array_push($this->set, $obj);
+            return 1;
+        }
+    }
+
+    public function delete($obj){
+        $pos =array_search($this->set, $obj);
+        if($pos === false) return 1;
+        unset($this->set[$pos]);
+        return 1;
+    }
+
+    public function getArray(){
+        return $this->set;
+    }
+
+//    public function __
+}
