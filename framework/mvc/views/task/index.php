23eb1295ad83192e5658191d7facd3e23a0b8567
Alex <591647088@qq.com>
2018-05-29 16:03:23 +0800

    typo: fix small bugs

--- framework/mvc/views/task/index.php
+++ framework/mvc/views/task/index.php
@@ -85,7 +85,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
 <script>window.jQuery || document.write('<script src="js/jquery-1.11.0.min.js"><\/script>')</script>
-<script src="http://cdn.alexfu.cc/js/{{site}}/js/ddsort.js"></script>
+<script src="http://cdn.alexfu.cc/js/{{site}}/ddsort.js"></script>
 <script>
     $( '#wrap' ).DDSort({
         target: 'li',
